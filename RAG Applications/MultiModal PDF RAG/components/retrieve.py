e28afb056842327bf6a59bb30afcff2d6a643dae
Musashi544 <akhileshkumars020911@gmail.com>
2025-09-07 00:17:12 +0530

    remove components folder

--- RAG Applications/MultiModal PDF RAG/components/retrieve.py
+++
@@ -1,7 +0,0 @@
-def retrieve(query, similarity_retriever, bm25_retriever): 
-    similarity_response = similarity_retriever.invoke(query) 
-    bm25_response = bm25_retriever.invoke(query)
-    
-    response = similarity_response + bm25_response 
-    
-    return response
